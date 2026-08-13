83dd624c5f52fe556deca32cff9d9c80bb9e00d7
Musa Mahlangu <125701914+Musamahlangu1977@users.noreply.github.com>
2026-08-13 23:23:08 +0200

    Configure website for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return {base:'/refilwe-thapelo-trading-projects/', 
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
